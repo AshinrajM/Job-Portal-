8f79b8d613b190a8091000458197d9524526a79d
AshinrajM <ashinrajeevank@outlook.com>
2024-10-03 15:30:35 +0530

    model company created with foreign key to user

--- job/models.py
+++ job/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Company(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        limit_choices_to={"role": "Employer"},
+        related_name="companies",
+    )
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
